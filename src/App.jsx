e57e26016f286940d3da82674198405b2394db7b
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-02 16:25:35 -0300

    add textarea to edit markdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
+import { initialMarkdown } from './utils/constants/initialMarkdown'
 
 const App = () => {
+  const [markdown, setMarkdown] = useState(initialMarkdown)
+
+  const handleInputChange = (event) => {
+    const newMarkdown = event.target.value
+    setMarkdown(newMarkdown)
+  }
+
   return (
     <div>
       <h1>Hello world 👋</h1>
+      <textarea
+        id="editor"
+        value={markdown}
+        onChange={handleInputChange}
+        placeholder="Enter your markdown here..."
+      ></textarea>
     </div>
   )
 }
