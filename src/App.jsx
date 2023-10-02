dd9760145b431dc4231d603b20bc81974b3943a6
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-02 16:28:35 -0300

    add div to preview markdown parsed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 import './App.css'
 import { initialMarkdown } from './utils/constants/initialMarkdown'
+import { marked } from 'marked'
 
 const App = () => {
   const [markdown, setMarkdown] = useState(initialMarkdown)
+
+  const parsedMarkdown = marked(markdown, { breaks: true })
 
   const handleInputChange = (event) => {
     const newMarkdown = event.target.value
@@ -19,6 +22,7 @@ const App = () => {
         onChange={handleInputChange}
         placeholder="Enter your markdown here..."
       ></textarea>
+      <div id="preview" dangerouslySetInnerHTML={{ __html: parsedMarkdown }} />
     </div>
   )
 }
