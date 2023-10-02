1a7d91aaaa0b2c8abf9daa5c41abac34bc9af7e8
ivanherreradev <herreraivan@outlook.com.ar>
2023-10-02 16:26:04 -0300

    add initialMarkdown constant

---
+++ src/utils/constants/initialMarkdown.js
@@ -0,0 +1,20 @@
+export const initialMarkdown = `# Welcome to My Markdown Previewer!
+
+## This is a Subheading
+Here's a [link](https://www.example.com).
+
+\`This is inline code.\`
+
+\`\`\`
+// This is a code block
+const example = 'Hello, world!';
+\`\`\`
+
+- List item 1
+- List item 2
+
+> This is a blockquote.
+
+![Markdown Logo](https://markdown-here.com/img/icon256.png)
+
+**This is bold text.**`
